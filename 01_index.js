c192b98c553b107a58e45d5764c5ce533f4e5d8b
Mohdumar09 <177651398+Mohdumar09@users.noreply.github.com>
2024-12-31 13:47:17 +0530

    Add files via upload

---
+++ 01_index.js
@@ -0,0 +1,39 @@
+// variables  in java script ....
+
+
+/*
+ there are three variables
+
+ 1 => let   // in this it can be only updated ,cannot be redeclared 
+ 2 => const // it cannot be updated and cannot be redeclared 
+ 3 => var // it can be updated and can be redeclared  
+
+*/
+
+
+
+// data types  in java script ....
+
+/*
+// string => "Umar"    ..> type =>  string
+// Number => 123        ..> type =>  number
+// boolean => true/false  ..> type =>  boolean
+// bigInt => for storing large number    ..> type =>  BigInt
+// Null => null      ..> type =>  object
+// undefined => value not assigned     ..> type =>  undefined
+*/
+
+
+/*  
+
+// conversion of data types 
+   
+// to convert into number  =>  Number;   (if alpahbet is inculde it return value NaN,
+if boolean = 0/1;
+if string = NaN )
+
+// to convert into boolean =>  Boolean (if string is converted =>  if empty = false/ if writtensomething =true
+if covert 0/1 into boolean ==> false/true return  )
+   
+// to  convert into string =>  String  
+*/
---
+++ 02_string.js
@@ -0,0 +1,45 @@
+/*
+//                                  ******************** STRINGS-prototype ***********************
+
+// str.charAt => return char at the given index..
+
+// str.charCodeAt => return the ascii code at the given index
+
+// str.codePointAt => works same as char COdeAt
+
+
+//  str.concat => adds the string 
+
+
+//  str.endsWith => check for the end char of the string is same as the given char 
+
+
+//  str.inculde => The includes() method of String values performs a case-sensitive search to determine whether a given string may be found within this string
+
+
+//  str.indexOf => give the index of the given char 
+
+
+// str.lastIndexOf => The lastIndexOf() method of String values searches this string and returns the index of the last occurrence of the specified substring
+
+
+// str.toUpperCase => convert to upper case
+
+
+// str.toLowerCase => convert to lower case
+
+
+// str.lenght => give the length of string .
+
+
+// str.replace =>  replace the given string with the currnt added string in function 
+
+
+// str.slice => cuts the string
+
+
+// str.split => cuts the string on the basis of given seperator .
+
+
+
+*/
